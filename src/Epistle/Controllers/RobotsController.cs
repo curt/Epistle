ff4bdd2bca52ca3e8a30b6e82a5bef3aef883b0b
Curt Gilman <14070+curt@users.noreply.github.com>
2024-04-26 05:03:37 -0700

    create appsettings key and value, robots controller and action

---
+++ src/Epistle/Controllers/RobotsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Epistle.Services;
+using Epistle.Domain.ViewModels;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Text;
+
+namespace Epistle.Controllers;
+
+[Route("/")]
+public class RobotsController(IConfiguration config) : ControllerBase
+{
+    [Route("robots.txt")]
+    public IActionResult RobotsTxt()
+    {
+        var response = new StringBuilder();
+        var arachnophobic = config.GetValue(typeof(bool), "Arachnophobic");
+
+        if (arachnophobic is not null && (bool)arachnophobic)
+        {
+            response.AppendLine("User-agent: *");
+            response.AppendLine("Disallow: / ");
+        }
+
+        return Content(response.ToString());
+    }
+}
